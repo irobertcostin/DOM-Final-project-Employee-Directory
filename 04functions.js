cdf7767c5e336c41b010c63c0b2c3c2eb9d03e08
irobertcostin <irobertcostin@gmail.com>
2022-11-13 20:23:26 +0200

    populate page through function

--- 04functions.js
+++ 04functions.js
@@ -24,7 +24,36 @@ function creazaCard (obj) {
     mainDiv.appendChild(expand)
     expand.classList.add("expand")
     expand.textContent = "EXPAND"
+    expand.classList.add("hide")
 
     return mainDiv;
 }
 
+
+// functie ce primeste ca parametru un vector nr carduri si nr de pagini
+
+function definestePagina (arr,nrCarduri,nrPagini){
+
+    let filter = [];
+
+    for(i=nrCarduri*(nrPagini-1);i<arr.length&&i<nrCarduri*nrPagini;i++){
+        filter.push(arr[i]);
+    }
+return filter;
+}
+
+
+// functie ce primeste ca parametru un arr si un obj, si returneaza arrayul
+
+function populeazaPagina (arr,nrPagina) {
+
+    arr = definestePagina(arr,12,nrPagina);
+    let population = document.querySelector(".population")
+    
+    for(i=0;i<arr.length;i++){
+        population.appendChild(creazaCard(arr[i]));
+    }
+
+    return arr;
+
+}
--- 05app.js
+++ 05app.js
@@ -1,2 +1,26 @@
 let population = document.querySelector(".population")
-population.appendChild(creazaCard(floor[0]));
+populeazaPagina(staff,1);
+
+
+population.addEventListener("mouseover", (e)=> {
+    
+    let obj=e.target
+    
+        if(obj.classList.contains("maindiv")){
+            obj.lastElementChild.classList.remove("hide")
+        }
+    
+})
+
+population.addEventListener("mouseleave", (e)=> {
+    let obj=e.target
+    
+        if(obj.classList.contains("maindiv")){
+            obj.lastElementChild.classList.add("hide")
+            
+        }
+
+})
+
+// event listener pe hover 
+
